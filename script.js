f18fe8a58f0c0b73faa1abebf4ff6e874d5e32ed
Buh90 <83811198+Buh90@users.noreply.github.com>
2023-04-28 19:12:02 +0200

    Fix display overflow

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const resetBtn = document.querySelector("#reset");
 const deleteBtn = document.querySelector("#delete");
 const oppositeBtn = document.querySelector("#opposite");
 const sqrtBtn = document.querySelector("#sqrt");
+const factorialBtn = document.querySelector("#factorial");
 
 let isOn = false;
 let isEqualPressed = false;
@@ -31,11 +32,25 @@ function reset() {
 // Display input
 for (let i = 0; i < buttons.length; i++) {
   if (buttons[i].classList.contains("digit")) {
-    buttons[i].addEventListener("click", typeNumber);
+    buttons[i].addEventListener("click", function () {
+      let btn = this.textContent;
+      typeNumber(btn);
+    });
   }
 }
 
-function typeNumber() {
+document.addEventListener("keypress", (e) => {
+  let keyboardInput = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+  let keyboardOperation = ["+", "-", "*", "/"];
+  if (keyboardInput.includes(e.key)) {
+    typeNumber(e.key);
+  }
+  if (keyboardOperation.includes(e.key)) {
+    typeNumber(e.key);
+  }
+});
+
+function typeNumber(digit) {
   if (!isOn) return;
   if (isEqualPressed) {
     reset();
@@ -43,8 +58,8 @@ function typeNumber() {
     isEqualPressed = false;
   }
   if (currentInput.length < 13) {
-    if (this.textContent === "." && String(currentInput).includes(".")) return;
-    currentInput += `${this.textContent}`;
+    if (digit === "." && String(currentInput).includes(".")) return;
+    currentInput += `${digit}`;
     displayInputLine.textContent = currentInput;
   }
 }
@@ -59,13 +74,6 @@ deleteBtn.onclick = () => {
     currentInput = currentInput.substr(0, currentInput.length - 1);
     displayInputLine.textContent = currentInput;
   }
-};
-
-// Opposite button
-oppositeBtn.onclick = () => {
-  result = Number(displayInputLine.textContent) * -1;
-  displayInputLine.textContent = result;
-  console.log(result, currentInput);
 };
 
 // Main operations
@@ -88,13 +96,14 @@ function selectOperation() {
     storedInput = result;
     currentInput = "";
     displayExpressionLine.textContent = storedInput + operationSymbol;
-    operation.push(this.getAttribute("id"));
-    operation.shift();
   } else {
-    operation.push(this.getAttribute("id"));
-    operation.shift();
     displayExpressionLine.textContent += currentInput + operationSymbol;
+    checkOperationSymbolNumber();
+    checkExpressionLineLenght(displayExpressionLine.textContent);
   }
+
+  operation.push(this.getAttribute("id"));
+  operation.shift();
 
   if (storedInput === "") {
     storedInput = currentInput;
@@ -111,7 +120,7 @@ function selectOperation() {
 }
 
 equalBtn.addEventListener("click", function () {
-  if (!isOn) return;
+  if (!isOn || isEqualPressed) return;
   if (storedInput == "") {
     result = currentInput;
   } else {
@@ -119,6 +128,7 @@ equalBtn.addEventListener("click", function () {
   }
   displayInputLine.textContent = result;
   displayExpressionLine.textContent += `${currentInput}=`;
+  checkExpressionLineLenght(displayExpressionLine.textContent);
   isEqualPressed = true;
 });
 
@@ -138,6 +148,7 @@ function calculate(a, b, c) {
       sum(a, b);
       break;
   }
+  result = checkResultLenght(result);
 }
 
 function divide(a, b) {
@@ -148,6 +159,7 @@ function divide(a, b) {
   }
   return result;
 }
+
 function multiply(a, b) {
   result = a * b;
   return result;
@@ -164,4 +176,73 @@ function sum(a, b) {
 }
 
 // Other operations
-sqrtBtn.onclick = () => {};
+
+oppositeBtn.onclick = () => {
+  result = Number(displayInputLine.textContent) * -1;
+  currentInput = result;
+  displayInputLine.textContent = result;
+};
+
+sqrtBtn.onclick = () => {
+  result = Number(displayInputLine.textContent);
+  if (result < 0) {
+    displayInputLine.textContent = "error";
+    reset();
+  } else {
+    result = Math.sqrt(result);
+    displayInputLine.textContent = result;
+    storedInput = result;
+  }
+  isEqualPressed = true;
+};
+
+factorialBtn.onclick = () => {
+  result = Number(displayInputLine.textContent);
+  if (result % 1 !== 0) {
+    displayInputLine.textContent = "only integer";
+    reset();
+  } else {
+    if (result === 0 || result === 1) return 1;
+    for (let i = result - 1; i >= 1; i--) {
+      result *= i;
+    }
+    displayInputLine.textContent = result;
+    storedInput = result;
+  }
+  isEqualPressed = true;
+};
+
+// Control function
+function checkOperationSymbolNumber() {
+  let displayString = displayExpressionLine.textContent;
+  let displayLastChar = displayString.toString().slice(-2, -1);
+  if (
+    displayLastChar === "+" ||
+    displayLastChar === "-" ||
+    displayLastChar === "x" ||
+    displayLastChar === "÷"
+  ) {
+    displayExpressionLine.textContent =
+      displayString.slice(0, -2) + displayString.slice(-1);
+  }
+}
+
+function checkResultLenght(result) {
+  let resultString = String(result).length;
+  if (resultString > 13) {
+    result = result.toExponential(7);
+  }
+  return result;
+}
+
+function checkExpressionLineLenght(exp) {
+  if (exp.length > 29) {
+    exp = "..." + exp.slice(exp.length - 26);
+  }
+  displayExpressionLine.textContent = exp;
+}
+
+// mettere input tastiera
+// funzioni mancanti
+// riduzione opacità con dito sul sensore
+// spegnimento automatico
